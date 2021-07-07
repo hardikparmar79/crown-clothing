b9d447c4ed443ff2b4a5a1f0e3264ffd9e7539fb
hardikparmar79 <76197771+hardikparmar79@users.noreply.github.com>
2021-07-07 11:40:10 +0530

    adding with route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
+import Homepage from './pages/homepage/homepage';
+
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-
 import Directory from '../../components/directory/directory.component';
+import './homepage.scss'
 
-import './homepage.styles.scss';
-
-const HomePage = () => (
+const Homepage = () => (
   <div className='homepage'>
-    <Directory />
+    <Directory/>
   </div>
 );
 
-export default HomePage;
+export default Homepage;
